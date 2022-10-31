927323084358bed307711d76a6cbfa20b3001493
Areej <iamareej6@gmail.com>
2022-10-31 14:30:55 +0500

    new folder

--- FYP - Cypress/Areej/StoreTest_grouping/cypress/e2e/POMS/comment_addition.js
+++
@@ -1,7 +0,0 @@
-export class comment {
-  coment() {
-    cy.get("#ordermsg > .form-control")
-      .type("Comment added successfully")
-      .should("be.enabled");
-  }
-}
--- FYP - Cypress/Areej/StoreTest_grouping/cypress/e2e/POMS/confirn_order.js
+++
@@ -1,6 +0,0 @@
-export class confirm_order {
-  click() {
-    cy.get("#cart_navigation > .button > span").click();
-    cy.get(".box").should("be.visible");
-  }
-}
