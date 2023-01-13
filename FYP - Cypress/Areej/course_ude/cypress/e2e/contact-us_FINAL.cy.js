fa2f8847e67e321cc2b444111c023ff760ed3cf2
Areej <iamareej6@gmail.com>
2023-01-13 11:44:53 +0500

    modifications

--- FYP - Cypress/Areej/course_ude/cypress/e2e/contact-us_FINAL.cy.js
+++ FYP - Cypress/Areej/course_ude/cypress/e2e/contact-us_FINAL.cy.js
@@ -3,11 +3,20 @@
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         //cypress code
-        cy.visit("http://www.webdriveruniversity.com/")
-        cy.get('#contact-us').click() 
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        //cy.get('#contact-us').click() 
+        cy.get('[name="first_name"]').type("areej")
+        cy.get('[name="last_name"]').type("Fatima")
+        cy.get('[name="email"]').type("iamareej6@gmail.com")
+        cy.get('textarea.feedback-input').type("today is january 13, 2023, 1 day since i started this udemy course")
+        cy.get('[type="submit"]').click()
     });
 
-    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        //cypress code
+    it.only("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.get('[name="first_name"]').type("areej")
+        cy.get('[name="last_name"]').type("Fatima")
+        cy.get('textarea.feedback-input').type("today is january 13, 2023, 1 day since i started this udemy course")
+        cy.get('[type="submit"]').click()
     });
 })
