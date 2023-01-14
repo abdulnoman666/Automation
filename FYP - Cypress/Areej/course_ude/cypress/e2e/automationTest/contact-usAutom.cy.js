e7ef28330cfcdec2daff227b36f59e3645aea28f
Areej <iamareej6@gmail.com>
2023-01-14 16:01:46 +0500

    selectors

--- FYP - Cypress/Areej/course_ude/cypress/e2e/automationTest/contact-usAutom.cy.js
+++ FYP - Cypress/Areej/course_ude/cypress/e2e/automationTest/contact-usAutom.cy.js
@@ -3,11 +3,12 @@
 describe("Test Contact Us form via Automation Test Store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.automationteststore.com/");
-        cy.get('.info_links_footer > :nth-child(5) > a').click();
+        // cy.xpath("//[contains(@href, 'contact')]").click();
+        cy.get("a[href$='contact']").click();
         cy.get('#ContactUsFrm_first_name').type("Areej")
         cy.get('#ContactUsFrm_email').type("iamareej6@gmail.com")
         cy.get('#ContactUsFrm_enquiry').type("i dont really have any enquiry")
-        cy.get('.col-md-6 > .btn').click();
+        cy.get("button[title='Submit']").click();
 
     });
 })
